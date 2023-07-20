ab0d21ddf9dda2636f66a97424360a1e5a67b738
srafsan <srafsan98@tutanota.com>
2023-07-20 09:42:43 +0600

    Nothing

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,6 +1,6 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from "express";
 import route from "./common/routeNames";
-import {router as authRouter} from "./routes/auth";
+import { router as authRouter } from "./routes/auth";
 import bodyParser from "body-parser";
 
 const app: Express = express();
@@ -8,23 +8,23 @@ const port = process.env.PORT || 3000;
 
 app.use(
   bodyParser.urlencoded({
-      extended: true
+    extended: true,
   })
-)
+);
 
 // app.use()
 
 app.get(route.home.main, (req: Request, res: Response) => {
-    res.send(
-      `<div>
-            <h1>Welcome to my app</h1>
+  res.send(
+    `<div>
+            <h1>Welcome to Job Portal</h1>
             <a href=${route.auth.login}>Login</a>
         </div>`
-    );
+  );
 });
 
-app.use(route.home.main, authRouter)
+app.use(route.home.main, authRouter);
 
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
