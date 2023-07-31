673c2241ed6815bad7d31522a2ae99f34dd723ee
srafsan <srafsan98@tutanota.com>
2023-07-31 17:55:53 +0600

    Some changes

--- Backend/src/routes/recruiter.ts
+++ Backend/src/routes/recruiter.ts
@@ -1,16 +1,29 @@
 import express, { Request, Response, NextFunction, Router } from "express";
+import { v4 as uuidv4 } from "uuid";
 
 import route from "../common/routeNames";
+import { insertJobToDB } from "../services/dbServices";
+import { statusMessages } from "../common/constants";
 
 const recruiterRoute: Router = Router();
 
 // Recruiter Post
-recruiterRoute.post(route.recruiter.addJob, (req: any, res: any) => {
-  //   const data = req.body;
-  console.log(req.body);
-  console.log("Job added successfully");
+recruiterRoute.post(route.recruiter.addJob, async (req: any, res: any) => {
+  const { name, description, image } = req.body;
 
-  res.send("Add Job Page");
+  const newJob: any = {
+    jobId: uuidv4(),
+    name,
+    description,
+    image,
+  };
+
+  try {
+    await insertJobToDB(newJob);
+    res.sendStatus(200);
+  } catch {
+    res.send(statusMessages[404]).sendStatus(404);
+  }
 });
 
 export { recruiterRoute };
