aeef53e1c961f191dedf1af25e9b7a3ac2e86e19
srafsan <srafsan98@tutanota.com>
2023-08-02 18:07:16 +0600

    Updates

--- frontend/src/app/dashboard/recruiter/manageJobs/page.tsx
+++ frontend/src/app/dashboard/recruiter/manageJobs/page.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+"use client";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const ManageJobs = () => {
-  return (
-    <div>
-      This is Managing Jobs
-    </div>
-  );
+  const [jobs, setJobs] = useState();
+
+  useEffect(() => {
+    async function fetchJobs() {
+      const data = axios.get("https://localhost:3001/");
+    }
+
+    fetchJobs();
+  }, []);
+
+  return <div>This is Managing Jobs</div>;
 };
 
 export default ManageJobs;
