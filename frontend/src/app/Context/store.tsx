02287f0271fa3a74c04136aef43cd2939ba8c054
srafsan <srafsan98@tutanota.com>
2023-08-03 18:06:18 +0600

    Updates

---
+++ frontend/src/app/Context/store.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import {
+  createContext,
+  useContext,
+  Dispatch,
+  SetStateAction,
+  useState,
+} from "react";
+
+type DataType = {
+  firstName: string;
+};
+
+interface ContextProps {
+  userId: string;
+  setUserId: Disp;
+}
